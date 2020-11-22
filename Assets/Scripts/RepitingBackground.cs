edc2056de5f9f147a0750cb28fdad03bd2a48ee9
santi <josevegasantiago8919@gmail.com>
2020-11-22 02:17:19 -0300

    Se puso el backgroun con movimiento

---
+++ Assets/Scripts/RepitingBackground.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepitingBackground : MonoBehaviour
+{
+    public float velocidad;
+    public float posicioonInicialX;
+    public float posicionFinalX;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * velocidad * Time.deltaTime);
+        if (transform.position.x < posicionFinalX)
+        {
+            Vector2 posicion = new Vector2(posicioonInicialX, transform.position.y);
+            transform.position = posicion;
+        }
+    }
+}
