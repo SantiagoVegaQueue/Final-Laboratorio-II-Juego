605e477bc158e34c76cb9c9e75f6ffc0b537c608
santi <josevegasantiago8919@gmail.com>
2020-11-24 21:49:45 -0300

    Puntaje y vida agregados

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class Player : MonoBehaviour
 {
     private Vector2 posicion;
@@ -14,6 +14,7 @@ public class Player : MonoBehaviour
 
     public int vida = 3;
     private Shake shake;
+    public Text vidaPantalla;
 
     private void Start()
     {
@@ -22,6 +23,7 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        vidaPantalla.text = vida.ToString();
         if (vida <= 0)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
